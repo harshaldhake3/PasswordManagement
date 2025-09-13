44bb495e4f1c4a12848a66304fc1a65806aa2912
harshaldhake3 <84280945+harshaldhake3@users.noreply.github.com>
2025-09-13 23:39:47 +0530

    Update seed-admin-h2.sql

--- src/main/resources/db/seed/seed-admin-h2.sql
+++ src/main/resources/db/seed/seed-admin-h2.sql
@@ -1,3 +1,3 @@
 -- H2: make an existing user admin (replace username)
 INSERT INTO user_roles(user_id, role)
-SELECT id, 'ADMIN' FROM app_user WHERE username='youradmin';
+SELECT id, 'ADMIN' FROM app_user WHERE username='admin';
